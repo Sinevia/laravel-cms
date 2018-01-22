a0bf1059e22768317372b9ef07903ca37c67dca1
Sinevia <info@sinevia.com>
2018-01-22 04:29:05 +0000

    Update Facade.php

--- src/Facade.php
+++ src/Facade.php
@@ -8,6 +8,6 @@ class Facade extends \Illuminate\Support\Facades\Facade
      */
     protected static function getFacadeAccessor()
     {
-        return 'knowledge';
+        return 'cms';
     }
 }
