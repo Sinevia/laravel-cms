802ba250ed3afb7d7ba072a1826a2e50c5585053
Sinevia <info@sinevia.com>
2018-01-25 23:31:02 +0000

    Update cms.php

--- config/cms.php
+++ config/cms.php
@@ -6,6 +6,9 @@
 return [
     // automatic loading of routes through main service provider
     'routes' => true,
+    // layout where the CMS will show into, i.e. admin.layouts.master
+    'layout-master' => '',
+    // URLs
     'urls' => [
         'media-manager' => '/your-media-manager-url',
     ]
