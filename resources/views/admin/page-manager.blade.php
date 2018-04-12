0d5ea57682886a846371a0fe00edb16bec48979a
Sinevia <info@sinevia.com>
2018-04-12 05:54:05 +0100

    Update page-manager.blade.php

--- resources/views/admin/page-manager.blade.php
+++ resources/views/admin/page-manager.blade.php
@@ -7,6 +7,10 @@
 @section('webpage_header')
 <h1>
     Page Manager
+    <button type="button" class="btn btn-primary pull-right" onclick="showPageCreateModal();">
+        <span class="glyphicon glyphicon-plus-sign"></span>
+        Add Page
+    </button>
 </h1>
 <ol class="breadcrumb">
     <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
