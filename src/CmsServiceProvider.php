516af7893f0057a7ca2b75317be931eecd9c7a7d
Milan Lesichkov <lesichkovm@gmail.com>
2020-06-06 12:00:41 +0100

    Update CmsServiceProvider.php

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -12,11 +12,14 @@ class CmsServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
+        $this->loadMigrationsFrom(dirname(__DIR__) . '/database/migrations');
+        $this->loadViewsFrom(dirname(__DIR__) . '/resources/views', 'cms');
+        //$this->loadRoutesFrom(dirname(__DIR__).'/routes.php'),         
+
         $this->publishes([
             dirname(__DIR__) . '/config/cms.php' => config_path('cms.php'),
-            $this->loadMigrationsFrom(dirname(__DIR__) . '/database/migrations'),
-            $this->loadViewsFrom(dirname(__DIR__) . '/resources/views', 'cms'),
-            //$this->loadRoutesFrom(dirname(__DIR__).'/routes.php'),            
+            dirname(__DIR__) . '/database/migrations' => database_path('migrations'),
+            dirname(__DIR__) . '/resources/views' => resource_path('views/cms'),
         ]);
     }
 
