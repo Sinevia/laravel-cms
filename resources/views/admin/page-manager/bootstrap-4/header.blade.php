6071305cdb0b7f3fe0ecedaacbd0d1e8418173cd
Sinevia <info@sinevia.com>
2021-07-02 06:23:32 +0100

    Create header.blade.php

---
+++ resources/views/admin/page-manager/bootstrap-4/header.blade.php
@@ -0,0 +1,12 @@
+<h1>
+    Page Manager
+    <button type="button" class="btn btn-primary float-right" onclick="showPageCreateModal();">
+        <span class="fa fa-plus-circle"></span>
+        Add Page
+    </button>
+</h1>
+<ol class="breadcrumb">
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminPageManager(); ?>">CMS</a></li>
+    <li class="active"><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminPageManager(); ?>">Pages</a></li>
+</ol>
