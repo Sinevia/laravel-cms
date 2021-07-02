d6f893936ae1538a32d672c714165a082543e5af
Sinevia <info@sinevia.com>
2021-07-02 07:28:06 +0100

    Update navigation.blade.php

--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -5,47 +5,8 @@ $templateCount = \Sinevia\Cms\Models\Template::where('Status', '<>', 'Deleted')-
 $translationCount = \Sinevia\Cms\Models\TranslationValue::count();
 $widgetCount = \Sinevia\Cms\Models\Widget::where('Status', '<>', 'Deleted')->count();
 ?>
-<div class="panel panel-default">
-    <div class="panel-body" style="padding: 2px;">
-        <ul class="nav nav-pills">
-            <li>
-                <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>">Dashboard</a>
-            </li>
-            <li>
-                <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminTemplateManager(); ?>">
-                    Templates
-                    <span class="badge"><?php echo $templateCount; ?></span>
-                </a>
-            </li>
-            <li>
-                <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminPageManager(); ?>">
-                    Pages
-                    <span class="badge"><?php echo $pageCount; ?></span>
-                </a>
-            </li>
-            <li>
-                <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminBlockManager(); ?>">
-                    Blocks
-                    <span class="badge"><?php echo $blockCount; ?></span>
-                </a>
-            </li>
-            <li>
-                <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminWidgetManager(); ?>">
-                    Widgets
-                    <span class="badge"><?php echo $widgetCount; ?></span>
-                </a>
-            </li>
-            <li>
-                <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminTranslationManager(); ?>">
-                    Translations
-                    <span class="badge"><?php echo $translationCount; ?></span>
-                </a>
-            </li>
-            <li>
-                <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminMediaManager(); ?>" target="_blank">
-                    Media
-                </a>
-            </li>
-        </ul>
-    </div>
-</div>
+<?php if (config("cms.bootstrap-version") == 5) { ?>
+    @include('cms::shared/navigation/bootstrap-5/navigation')
+<?php } else { ?>
+    @include('cms::shared/navigation/bootstrap-4/navigation')
+<?php } ?>
