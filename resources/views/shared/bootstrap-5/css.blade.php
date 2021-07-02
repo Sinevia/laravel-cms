e924e08c506f88d3c21e6ff916ef2f363aebc9df
Sinevia <info@sinevia.com>
2021-07-02 07:29:16 +0100

    Create css.blade.php

---
+++ resources/views/shared/bootstrap-5/css.blade.php
@@ -0,0 +1,32 @@
+<style>
+    .well {
+        min-height: 20px;
+        padding: 19px;
+        margin-bottom: 20px;
+        background-color: #fafafa;
+        border: 1px solid #e8e8e8;
+        border-radius: 0;
+        -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,0.05);
+        box-shadow: inset 0 1px 1px rgba(0,0,0,0.05);
+    }
+/*    .form-group {
+        margin-bottom: 1rem;
+    }
+
+    .form-inline .form-control {
+        display: inline-block;
+        width: auto;
+        vertical-align: middle;
+    }
+
+    .form-row {
+        display: flex;
+        flex-wrap: wrap;
+        margin-right: -5px;
+        margin-left: -5px;
+    }
+
+    label {
+        margin-bottom: 0.5rem;
+    }*/
+</style>
