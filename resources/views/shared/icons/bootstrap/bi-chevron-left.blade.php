61bc665ed1d08ffb5cb5768d225d9c60646da2a7
Sinevia <info@sinevia.com>
2021-07-03 05:45:38 +0100

    Create bi-chevron-left.blade.php

---
+++ resources/views/shared/icons/bootstrap/bi-chevron-left.blade.php
@@ -0,0 +1,3 @@
+<svg @include("cms::shared/icons/bootstrap/attributes", ['class'=>'bi-check-circle' . ($class ?? '')])>
+  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+</svg>
