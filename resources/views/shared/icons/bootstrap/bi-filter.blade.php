e248d73414f48ca128882b48279a616421b69fa4
Sinevia <info@sinevia.com>
2021-07-03 05:39:16 +0100

    Update bi-filter.blade.php

--- resources/views/shared/icons/bootstrap/bi-filter.blade.php
+++ resources/views/shared/icons/bootstrap/bi-filter.blade.php
@@ -1,21 +1,3 @@
-<?php
-$width = $width ?? '16';
-$height = $height ?? '16';
-$fill = $fill ?? 'currentColor';
-$class = 'bi bi-info-circle-fill ' . ($class ?? '');
-$onclick = $onclick ?? '';
-
-$attributes = [
-    'width' => $width,
-    'height' => $height,
-    'fill' => $fill,
-    'class' => $class,
-    'viewbox' => '0 0 ' . $width . ' ' . $height,
-];
-if ($onclick != "") {
-    $attributes['onclick'] = $onclick;
-}
-?>
-<svg xmlns="http://www.w3.org/2000/svg" <?= \Sinevia\Cms\Helpers\CmsHelper::arrayToHtmlAttributes($attributes) ?>>
+<svg  @include("cms::shared/icons/bootstrap/attributes", ['class'=>'bi-filter' . ($class ?? '')])>
     <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
 </svg>
