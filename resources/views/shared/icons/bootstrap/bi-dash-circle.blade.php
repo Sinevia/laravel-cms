0b7620e5f47b9f022e58f6f06d3960eb26c272ee
Sinevia <info@sinevia.com>
2021-07-03 07:36:03 +0100

    Create bi-dash-circle.blade.php

---
+++ resources/views/shared/icons/bootstrap/bi-dash-circle.blade.php
@@ -0,0 +1,4 @@
+<svg @include("cms::shared/icons/bootstrap/attributes", ['class'=>'bi-dash-circle' . ($class ?? '')])>
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+</svg>
