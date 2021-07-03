7144f9e13a5fb1e681da9b37d7075e125315d187
Sinevia <info@sinevia.com>
2021-07-03 05:38:47 +0100

    Update bi-plus-circle.blade.php

--- resources/views/shared/icons/bootstrap/bi-plus-circle.blade.php
+++ resources/views/shared/icons/bootstrap/bi-plus-circle.blade.php
@@ -1,22 +1,4 @@
-<?php
-$width = $width ?? '16';
-$height = $height ?? '16';
-$fill = $fill ?? 'currentColor';
-$class = 'bi bi-info-circle-fill ' . ($class ?? '');
-$onclick = $onclick ?? '';
-
-$attributes = [
-    'width' => $width,
-    'height' => $height,
-    'fill' => $fill,
-    'class' => $class,
-    'viewbox' => '0 0 ' . $width . ' ' . $height,
-];
-if ($onclick != "") {
-    $attributes['onclick'] = $onclick;
-}
-?>
-<svg xmlns="http://www.w3.org/2000/svg" <?= \Sinevia\Cms\Helpers\CmsHelper::arrayToHtmlAttributes($attributes) ?>>
+<svg  @include("cms::shared/icons/bootstrap/attributes", ['class'=>'bi-plus-circle' . ($class ?? '')])>
     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
 </svg>
