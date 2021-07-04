aaf1a1b61f367eba9d3baf456f1662342ea37c84
Sinevia <info@sinevia.com>
2021-07-04 12:48:37 +0100

    Create header.blade.php

---
+++ resources/views/admin/block-manager/bootstrap-4/header.blade.php
@@ -0,0 +1,13 @@
+<h1>
+    Block Manager
+    <button type="button" class="btn btn-primary pull-right" onclick="showBlockCreateModal();">
+        <span class="glyphicon glyphicon-plus-sign"></span>
+        Add Block
+    </button>
+</h1>
+
+<ol class="breadcrumb">
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome() ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminBlockManager(); ?>">Blocks</a></li>
+    <li class="active"><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminBlockManager(); ?>">Blocks</a></li>
+</ol>
