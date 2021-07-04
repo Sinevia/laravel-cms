742fda77f8f0398b36e2fa4f53f77ef2ace9eb2c
Sinevia <info@sinevia.com>
2021-07-04 21:07:23 +0100

    Create header.blade.php

---
+++ resources/views/admin/translation-manager/bootstrap-4/header.blade.php
@@ -0,0 +1,12 @@
+<h1>
+    Translation Manager
+    <button type="button" class="btn btn-primary pull-right" onclick="showTranslationCreateModal();">
+        <span class="glyphicon glyphicon-plus-sign"></span>
+        Add Translation
+    </button>
+</h1>
+<ol class="breadcrumb">
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminTranslationManager(); ?>">CMS</a></li>
+    <li class="active"><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminTranslationManager(); ?>">Translations</a></li>
+</ol>
