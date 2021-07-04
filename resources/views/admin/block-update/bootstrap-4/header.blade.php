7be861a5ff4878b3cfd6856d7f992451ab35ea4d
Sinevia <info@sinevia.com>
2021-07-04 13:11:16 +0100

    Create header.blade.php

---
+++ resources/views/admin/block-update/bootstrap-4/header.blade.php
@@ -0,0 +1,13 @@
+<section class="content-header">
+    <h1>
+        Edit Block: <?php echo $block->Title; ?>
+        <small>(#<?php echo $block->Id; ?>)</small>
+    </h1>
+
+    <ol class="breadcrumb">
+        <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome() ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminBlockManager(); ?>">Blocks</a></li>
+        <li class="active"><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminBlockManager(); ?>">Blocks</a></li>
+        <li class="active">Edit Block</li>
+    </ol>
+</section>
