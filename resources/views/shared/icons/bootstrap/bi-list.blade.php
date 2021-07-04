22d3886a88b531c361376aff8308a60621ffd245
Sinevia <info@sinevia.com>
2021-07-04 19:37:54 +0100

    Create bi-list.blade.php

---
+++ resources/views/shared/icons/bootstrap/bi-list.blade.php
@@ -0,0 +1,3 @@
+<svg  @include("cms::shared/icons/bootstrap/attributes", ['class'=>'bi-list' . ($class ?? '')])>
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg>
