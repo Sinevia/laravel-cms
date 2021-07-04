e43d062c308df1e0225f9a6ba698159ed01ae949
Sinevia <info@sinevia.com>
2021-07-04 16:26:32 +0100

    Create header.blade.php

---
+++ resources/views/admin/template-manager/bootstrap-4/header.blade.php
@@ -0,0 +1,13 @@
+<h1>
+    Template Manager
+    <button type="button" class="btn btn-primary pull-right" onclick="showTemplateCreateModal();">
+        <span class="glyphicon glyphicon-plus-sign"></span>
+        Add Template
+    </button>
+</h1>
+
+<ol class="breadcrumb">
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>">CMS</li>
+    <li class="active"><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminTemplateManager(); ?>">Templates</li>
+</ol>
