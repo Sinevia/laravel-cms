e348f21c438a0aee05a0c22a0dfa5edaf7393dcc
Sinevia <info@sinevia.com>
2021-07-04 20:39:58 +0100

    Create header.blade.php

---
+++ resources/views/admin/widget-update/bootstrap-4/header.blade.php
@@ -0,0 +1,10 @@
+<h1>
+    Edit Widget: <?php echo $widget->Title; ?>
+    <small>(#<?php echo $widget->Id; ?>)</small>
+</h1>
+<ol class="breadcrumb">
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>">CMS</a></li>
+    <li class="active"><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminWidgetManager(); ?>">Widgets</a></li>
+    <li class="active">Edit Widget</li>
+</ol>
