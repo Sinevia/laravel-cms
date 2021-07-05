11ff4568b454579a3b6a51e7db848e0d1dedc6a7
Sinevia <info@sinevia.com>
2021-07-05 04:15:20 +0100

    Create header.blade.php

---
+++ resources/views/admin/translation-update/bootstrap-4/header.blade.php
@@ -0,0 +1,13 @@
+<h1>
+    Widget Manager
+    <button type="button" class="btn btn-primary pull-right" onclick="showWidgetCreateModal();">
+        <span class="glyphicon glyphicon-plus-sign"></span>
+        Add Widget
+    </button>
+</h1>
+
+<ol class="breadcrumb">    
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>">CMS</a></li>
+    <li class="active"><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminWidgetManager(); ?>">Widgets</a></li>
+</ol>
