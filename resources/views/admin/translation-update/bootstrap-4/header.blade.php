53c1bf041146b1cc22063d03b04a287549114755
Sinevia <info@sinevia.com>
2021-07-05 04:19:25 +0100

    Update header.blade.php

--- resources/views/admin/translation-update/bootstrap-4/header.blade.php
+++ resources/views/admin/translation-update/bootstrap-4/header.blade.php
@@ -1,13 +1,13 @@
-<h1>
-    Widget Manager
-    <button type="button" class="btn btn-primary pull-right" onclick="showWidgetCreateModal();">
-        <span class="glyphicon glyphicon-plus-sign"></span>
-        Add Widget
-    </button>
-</h1>
-
-<ol class="breadcrumb">    
-    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
-    <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>">CMS</a></li>
-    <li class="active"><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminWidgetManager(); ?>">Widgets</a></li>
-</ol>
+<section class="content-header">
+    <h1>
+        Edit Translation: <?php echo $translationKey->Title; ?>
+        <small>(#<?php echo $translationKey->Id; ?>)</small>
+    </h1>
+    
+    <ol class="breadcrumb">
+        <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>">CMS</a></li>
+        <li><a href="<?php echo \Sinevia\Cms\Helpers\Links::adminTranslationManager(); ?>">Translations</a></li>
+        <li class="active">Edit Translation</li>
+    </ol>
+</section>
