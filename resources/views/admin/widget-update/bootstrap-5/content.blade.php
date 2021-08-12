2b9400155c568cd1bdb78e00c59115cf8b2519a0
Sinevia <info@sinevia.com>
2021-08-12 18:18:15 +0100

    Update content.blade.php

--- resources/views/admin/widget-update/bootstrap-5/content.blade.php
+++ resources/views/admin/widget-update/bootstrap-5/content.blade.php
@@ -221,9 +221,9 @@
                         event.preventDefault();
                         $('#ButtonApply').trigger('click');
                         break;
+                }
+                return false;
             }
-            }
-            return false;
         });
     }, 500);
 </script>
