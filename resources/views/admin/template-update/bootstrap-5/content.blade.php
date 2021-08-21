a5e4c7a0841d16d539a04d13c0e01d6300a9bf8d
Sinevia <info@sinevia.com>
2021-08-21 05:23:06 +0100

    Update content.blade.php

--- resources/views/admin/template-update/bootstrap-5/content.blade.php
+++ resources/views/admin/template-update/bootstrap-5/content.blade.php
@@ -176,8 +176,6 @@
                         $('#ButtonApply').trigger('click');
                         break;
                 }
-                
-                return false;
             }
         });
     }, 500);
