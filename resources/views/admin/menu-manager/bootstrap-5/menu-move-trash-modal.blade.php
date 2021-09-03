f872281e6d8c3628ca9f8c4985d4d6c5dae8fe37
Sinevia <info@sinevia.com>
2021-09-03 22:11:37 +0100

    Create menu-move-trash-modal.blade.php

---
+++ resources/views/admin/menu-manager/bootstrap-5/menu-move-trash-modal.blade.php
@@ -0,0 +1,39 @@
+<div class="modal fade" id="ModalMenuMoveToTrash" tabindex="-1" aria-labelledby="ModalMenuMoveToTrashLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalMenuMoveToTrashLabel">
+                    Confirm Menu Move to Trash
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div>
+                    Are you sure you want to move this page to trash?
+                </div>
+
+                <form name="FORM_PAGE_MOVE_TO_TRASH" method="post" action="<?php echo \Sinevia\Cms\Helpers\Links::adminMenuMoveToTrash(); ?>">
+                    <input type="hidden" name="menu_id" value="">
+                    <?php echo csrf_field(); ?>
+                </form>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    @include("cms::shared/icons/bootstrap/bi-chevron-left")
+                    Close
+                </button>
+                <button type="button" class="btn btn-warning" onclick="FORM_PAGE_MOVE_TO_TRASH.submit();">
+                    @include("cms::shared/icons/bootstrap/bi-trash")
+                    Move to Trash
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function confirmMenuMoveToTrash(pageId) {
+        $('#ModalMenuMoveToTrash input[name=MenuId]').val(pageId);
+        $('#ModalMenuMoveToTrash').modal('show');
+    }
+</script>
