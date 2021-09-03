2847530156642c05489d2722ff16e95830e66fb1
Sinevia <info@sinevia.com>
2021-09-03 22:10:51 +0100

    Create menu-delete.blade.php

---
+++ resources/views/admin/menu-manager/bootstrap-5/menu-delete.blade.php
@@ -0,0 +1,43 @@
+<div class="modal fade" id="ModalMenuDelete" tabindex="-1" aria-labelledby="ModalMenuDeleteLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalMenuDeleteLabel">
+                    Confirm Menu Delete
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div>
+                    Are you sure you want to delete this page?
+                </div>
+                
+                <div class="text-danger">
+                    Note! This action cannot be undone.
+                </div>
+
+                <form name="FORM_PAGE_DELETE" method="post" action="<?php echo \Sinevia\Cms\Helpers\Links::adminMenuDelete(); ?>">
+                    <input type="hidden" name="menu_id" value="">
+                    <?php echo csrf_field(); ?>
+                </form>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    @include("cms::shared/icons/bootstrap/bi-chevron-left")
+                    Close
+                </button>
+                <button type="button" class="btn btn-danger" onclick="FORM_PAGE_DELETE.submit();">
+                    @include("cms::shared/icons/bootstrap/bi-x-circle")
+                    Delete
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function confirmMenuDelete(page_id) {
+        $('#ModalMenuDelete input[name=MenuId]').val(page_id);
+        $('#ModalMenuDelete').modal('show');
+    }
+</script>
