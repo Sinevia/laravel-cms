44514a742517ca12eb90bf631943f3eaa228c861
Sinevia <info@sinevia.com>
2021-09-04 08:27:45 +0100

    Update menuitem-manager.blade.php

--- resources/views/admin/menuitem-manager.blade.php
+++ resources/views/admin/menuitem-manager.blade.php
@@ -1,1 +1,29 @@
+<?php if (View::exists(config('cms.layout-master'))) { ?>
+    @extends(config('cms.layout-master'))
+<?php } ?>
 
+@section('webpage_title', 'Menu Item Manager')
+
+@section('webpage_header')
+<?php if (config("cms.bootstrap-version") == 5) { ?>
+    @include('cms::admin/menuitem-manager/bootstrap-5/header')
+<?php } else { ?>
+    @include('cms::admin/menuitem-manager/bootstrap-4/header')
+<?php } ?>
+@stop
+
+@section('webpage_content')
+
+<?php if (config("cms.bootstrap-version") == 5) { ?>
+    @include('cms::shared/bootstrap-5/css')
+<?php } ?>
+
+@include('cms::shared.navigation')
+
+<?php if (config("cms.bootstrap-version") == 5) { ?>
+    @include('cms::admin/menuitem-manager/bootstrap-5/content')
+<?php } else { ?>
+    @include('cms::admin/menuitem-manager/bootstrap-4/content')
+<?php } ?>
+
+@stop
