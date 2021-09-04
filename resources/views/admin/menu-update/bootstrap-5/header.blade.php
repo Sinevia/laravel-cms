92ead45d11278aa35bd2d6a1920748c439d54c0f
Sinevia <info@sinevia.com>
2021-09-04 05:58:14 +0100

    Create header.blade.php

---
+++ resources/views/admin/menu-update/bootstrap-5/header.blade.php
@@ -0,0 +1,23 @@
+<h1>
+    Edit Menu: 
+    {{ $menu->Title }}    
+    <small>(<?php echo $menu->Status; ?>)</small>
+</h1>
+
+<ol class="breadcrumb">
+    <li class="breadcrumb-item">
+        <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminHome(); ?>">
+            @include("cms::shared.icons.bootstrap.bi-house")
+            Home
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminMenuManager(); ?>">CMS</a>
+    </li>
+    <li class="breadcrumb-item">
+        <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminMenuManager(); ?>">Menus</a>
+    </li>    
+    <li class="breadcrumb-item active">
+        <a href="<?php echo \Sinevia\Cms\Helpers\Links::adminMenuManager(); ?>">Menu: <b>{{ $menu->Title }}</a>
+    </li>
+</ol>
