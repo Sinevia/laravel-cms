4fbf657d175d20555bba765dfba5e3899798ce33
Sinevia <info@sinevia.com>
2021-09-04 19:13:57 +0100

    Update BaseModel.php

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -3,6 +3,8 @@
 namespace Sinevia\Cms\Models;
 
 class BaseModel extends \AdvancedModel {
+    use \Illuminate\Database\Eloquent\SoftDeletes;
+    
     public $timestamps = true;
     public $useUniqueId = true;
 }
